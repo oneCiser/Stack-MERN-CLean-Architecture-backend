64b48ad851d736f6385e740c590f23e68c5eaacc
oneCiser <41345659+oneCiser@users.noreply.github.com>
2022-03-10 23:23:02 -0500

    fix: redis file import

--- src/database/redis/index.ts
+++ src/database/redis/index.ts
@@ -1,5 +1,5 @@
 import RedisStore from './middleware'
-import redisClient from './connections'
+import redisClient from './connection'
 const REDIS_HOST: string = process.env.REDIS_HOST || 'localhost';
 const REDIS_PORT: number = process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379;
 const store = new RedisStore({
