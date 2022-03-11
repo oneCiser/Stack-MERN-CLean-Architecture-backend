89f1f6adb3e31c2d438fe91967dcc688304a6144
oneCiser <41345659+oneCiser@users.noreply.github.com>
2022-03-10 23:04:56 -0500

    fix: dotevn

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,18 +1,18 @@
-// import dotenv from "dotenv";
-const dotenv = require("dotenv");
+// // import dotenv from "dotenv";
+// const dotenv = require("dotenv");
 
-const environment = process.env.NODE_ENV;
+// const environment = process.env.NODE_ENV;
 
-// config environment
-try {
-  dotenv.config();
-  // if (environment === "development") {
-  //   dotenv.config({ path: ".env.dev" });
-  // } else if (environment === "production") {
-  //   dotenv.config({ path: ".env.prod" });
-  // } else {
-  //   dotenv.config();
-  // }
-} catch (error: any) {
-  console.log("Dotenv config error: ", error);
-}
+// // config environment
+// try {
+//   dotenv.config();
+//   // if (environment === "development") {
+//   //   dotenv.config({ path: ".env.dev" });
+//   // } else if (environment === "production") {
+//   //   dotenv.config({ path: ".env.prod" });
+//   // } else {
+//   //   dotenv.config();
+//   // }
+// } catch (error: any) {
+//   console.log("Dotenv config error: ", error);
+// }
